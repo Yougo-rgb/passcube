2222083d89241d3f1f69cb6ec9ec73d01bf8e6f1
Hugo Pozzi <hugopozzi07@icloud.com>
2026-09-19 12:12:31 +0200

    Implement protocol functions and constants

--- passcube-web/src/protocol.js
+++ passcube-web/src/protocol.js
@@ -1,3 +1,14 @@
+const STX = 0x02;
+const ETX = 0x03;
+const ARDUINO_ADDRESS = 0x01;
+const XOR_KEY = 0x67;
+
+/**
+ * Describes the structure of a communication frame.
+ *
+ * A frame contains a start byte, destination address, body length,
+ * body data, checksum, and end byte.
+ */
 const frameStructure = {
   stx: {
     name: "STX",
@@ -41,12 +52,46 @@ const frameStructure = {
   },
 };
 
+/**
+ * Calculates the XOR checksum of a frame.
+ *
+ * The checksum is calculated using the address, length, and every byte contained in the body.
+ *
+ * @param {number} address - The destination device address.
+ * @param {number} length - The number of bytes in the body.
+ * @param {Uint8Array} body - The body bytes.
+ * @returns {number} The calculated checksum.
+ */
 function claculateChecksum(address, length, body) {
-  // TODO
+  let checksum = address ^ length;
+
+  for (const byte of body) {
+    checksum ^= byte;
+  }
+
+  return checksum;
 }
 
-function buildFrame(body, address = ARDUINO_ADDRESS) {
-  // TODO
+/**
+ * Builds a communication frame according to the defined protocol.
+ *
+ * @param {number} address - The destination device address.
+ * @param {Uint8Array} body - The command and associated data.
+ * @returns {Uint8Array} The complete communication frame.
+ */
+function buildFrame(address = ARDUINO_ADDRESS, body) {
+  const length = body.length;
+  const checksum = claculateChecksum(address, length, body);
+
+  return new Uint8Array([STX, address, length, ...body, checksum, ETX]);
 }
 
-export { buildFrame, claculateChecksum, frameStructure };
+export {
+  buildFrame,
+  claculateChecksum,
+  frameStructure,
+  STX,
+  ETX,
+  ARDUINO_ADDRESS,
+  XOR_KEY
+};
